eb981f840a7371330be3c9518f31534776f4f9ff
Poojabhanudevops <poojabhanu16@gmail.com>
2025-04-04 23:14:10 +0530

    Create DryRun.groovy

---
+++ src/org/Common/src/org/ansible/CI /DryRun.groovy
@@ -0,0 +1,13 @@
+package org.ansible.CI
+
+def call (String inventory_path, String playbook_path, String key_path, String user_name ){
+  stage('Dry Run'){
+
+                script {
+                    catchError(buildResult: 'SUCCESS') {
+                        sh "ansible-playbook -C -D -i ${inventory_path} ${playbook_path} --private-key=${key_path} -u ${user_name}  > Report-dry-run.txt"
+                }
+            }
+      }
+  
+  }

831c51c9454fd83b94f29941769e246b3337b3ee
Poojabhanudevops <poojabhanu16@gmail.com>
2025-04-04 23:12:07 +0530

    Create CloneRepo.groovy

---
+++ src/org/Common/src/org/Common/CloneRepo.groovy
@@ -0,0 +1,12 @@
+package org.Common
+
+def call(String branch_name, String repo_url, String git_password) {
+
+stage('Clone Repo') {
+        checkout([$class: 'GitSCM',
+                    branches: [[name: "*/${branch_name}"]],
+                    userRemoteConfigs: [[url: repo_url , credentialsId: git_password ]]
+                    ])
+          }
+
+}
